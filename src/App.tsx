fbaa1b51a493ef2d63394368fa39e9bdb447ad1a
behzad <behzadaminipour87@gmail.com>
2024-02-05 15:38:27 +0330

    Defining Component Props Types

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import CourseGoal from "./components/CourseGoal";
 
 function App() {
   return (
-    <main className="max-w-4xl mx-auto">
-      <h1 className="text-blue-500">Hello typescript</h1>
-      <CourseGoal />
+    <main className="max-w-4xl mx-auto px-2">
+      <h1 className="text-red-500">Hello typescript</h1>
+      <CourseGoal title="Learn TS" description="Crash course for learning TS" />
     </main>
   );
 }
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,19 @@
-const CourseGoal = () => {
+const CourseGoal = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
   return (
     <article>
       <div>
-        <h2>Title</h2>
-        <p>Description</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
-      <button>Delete</button>
+      <button className="p-1 bg-red-500 text-slate-50 rounded-md px-2 mt-2">
+        Delete
+      </button>
     </article>
   );
 };
