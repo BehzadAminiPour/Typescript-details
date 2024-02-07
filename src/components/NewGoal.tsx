11607516f8d60749de66a2b186c813d8cfbff9ad
behzad <behzadaminipour87@gmail.com>
2024-02-07 12:05:32 +0330

    Using useRef

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,8 +1,13 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const NewGoal = () => {
-  const handleSubmit = (event: FormEvent) => {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -10,13 +15,13 @@ const NewGoal = () => {
         <label className="mr-2" htmlFor="goal">
           Your Goal
         </label>
-        <input className="rounded-md" type="text" id="goal" />
+        <input className="rounded-md" type="text" id="goal" ref={goal} />
       </p>
       <p className="my-2">
         <label className="mr-2" htmlFor="summary">
           Short Summary
         </label>
-        <input className="rounded-md" type="text" id="summary" />
+        <input className="rounded-md" type="text" id="summary" ref={summary} />
       </p>
       <p className="my-2">
         <button className="bg-green-700 px-2 text-slate-50 rounded-md">
